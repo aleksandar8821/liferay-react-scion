888b3643263ea77029ddcf16ccd46d91f6868aff
Aleksandar Foltin <aleksandar.foltin@panonit.com>
2021-10-13 13:56:00 +0200

    initial commit, main compoenent converted to function component, package json updated with scion specific packages

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-portlet-sample-data
+
+React project generated with Yo with sample data
---
+++ src/AppComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App () {
+		return (
+            <div>
+				<div>
+        	        <span className="tag">Portlet Namespace:</span> 
+					<span className="value">{this.props.portletNamespace}</span>
+				</div>
+				<div>
+    	            <span className="tag">Context Path:</span>
+					<span className="value">{this.props.contextPath}</span>
+				</div>
+				<div>
+	                <span className="tag">Portlet Element Id:</span>
+					<span className="value">{this.props.portletElementId}</span>
+				</div>
+				
+				<div>
+					<span className="tag">Configuration:</span>
+					<span className="value pre">{JSON.stringify(this.props.configuration, null, 2)}</span>
+				</div>
+				
+			</div>
+		);
+}
