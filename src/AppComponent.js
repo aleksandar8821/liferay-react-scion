27e1a0ecd9513cd7570c1092a87911eb92f4c13f
Aleksandar Foltin <aleksandar.foltin@panonit.com>
2021-10-14 09:43:41 +0200

    scion built, runned, error with router outlet

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -1,7 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {  MicrofrontendPlatform, OutletRouter, PlatformConfig } from '@scion/microfrontend-platform';
+import { Beans } from '@scion/toolkit/bean-manager';
 
-export default function App () {
+export default class extends React.Component {
+
+
+	// constants = {
+	// 	hostAppUrl: "http://localhost:4200",
+	// 	headerAppUrl: "http://localhost:4201",
+	// 	navbarAppUrl: "http://localhost:4202",
+	// 	capitalAppUrl: "http://localhost:4203",
+	// 	transactionAppUrl: "http://localhost:4204",
+	// 	chartAppUrl: "http://localhost:4205",
+	  
+	// 	manifest: "/o/react-portlet-sample-data/json/manifest.json",
+	// 	hostManifestRelativeURL: ""
+	//   };
+
+	//  platformConfig = [
+	// 	{symbolicName: 'host-app', manifestUrl: `${constants.hostManifestRelativeURL}/${constants.manifest}`},
+	// 	{symbolicName: 'header-app', manifestUrl: `${constants.headerAppUrl}/${constants.manifest}`},
+	// 	{symbolicName: 'navbar-app', manifestUrl: `${constants.navbarAppUrl}/${constants.manifest}`},
+	// 	{symbolicName: 'capital-app', manifestUrl: `${constants.capitalAppUrl}/${constants.manifest}`},
+	// 	{symbolicName: 'chart-app', manifestUrl: `${constants.chartAppUrl}/${constants.manifest}`},
+	// 	{symbolicName: 'transactions-app', manifestUrl: `${constants.transactionAppUrl}/${constants.manifest}`},
+	//   ];
+
+
+
+	  componentDidMount() {
+
+		const constants = {
+				hostAppUrl: "http://localhost:4200",
+				headerAppUrl: "http://localhost:4201",
+				navbarAppUrl: "http://localhost:4202",
+				capitalAppUrl: "http://localhost:4203",
+				transactionAppUrl: "http://localhost:4204",
+				chartAppUrl: "http://localhost:4205",
+			  
+				manifest: "o/react-portlet-sample-data/json/manifest.json",
+				hostManifestRelativeURL: ""
+			  };
+
+			 const 	 platformConfig = [
+		{symbolicName: 'host-app', manifestUrl: `${constants.hostManifestRelativeURL}/${constants.manifest}`},
+		// {symbolicName: 'header-app', manifestUrl: `${constants.headerAppUrl}/${constants.manifest}`},
+		// {symbolicName: 'navbar-app', manifestUrl: `${constants.navbarAppUrl}/${constants.manifest}`},
+		// {symbolicName: 'capital-app', manifestUrl: `${constants.capitalAppUrl}/${constants.manifest}`},
+		// {symbolicName: 'chart-app', manifestUrl: `${constants.chartAppUrl}/${constants.manifest}`},
+		// {symbolicName: 'transactions-app', manifestUrl: `${constants.transactionAppUrl}/${constants.manifest}`},
+	  ];
+
+		 async function init() {
+			// Start the platform
+			await MicrofrontendPlatform.startHost(platformConfig, {symbolicName: 'host-app'});
+		
+			Beans.get(OutletRouter).navigate(`http://localhost:4201/header-app.html`, {outlet: 'HEADER'});
+		
+			// Beans.get(OutletRouter).navigate(`${constants.navbarAppUrl}/navbar-app.html`, {outlet: 'NAVBAR'});
+		
+			// Beans.get(OutletRouter).navigate(`${constants.chartAppUrl}/index.html`, {outlet: 'MAIN-SCREEN-ASIDE'});
+		
+			// Beans.get(OutletRouter).navigate(`${constants.capitalAppUrl}/index.html`);
+		  }
+
+		  init();
+		console.log('mounted');
+	  }
+
+	render() {
 		return (
             <div>
 				<div>
@@ -21,7 +89,10 @@ export default function App () {
 					<span className="tag">Configuration:</span>
 					<span className="value pre">{JSON.stringify(this.props.configuration, null, 2)}</span>
 				</div>
+
+				<sci-router-outlet name="HEADER"></sci-router-outlet>
 				
 			</div>
 		);
+	}	
 }
